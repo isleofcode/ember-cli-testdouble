4fe5f38e277ce075596e5d29cb40752baeb6b2f6
Alex LaFroscia <alex@lafroscia.com>
2017-05-03 12:03:44 -0700

    Removes dependency on Bower

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-/* jshint node: true */
-'use strict';
+/* eslint-env node */
+
 var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+
+var tdDirectory = path.resolve(path.dirname(require.resolve('testdouble')), '..');
 
 module.exports = {
   name: 'ember-cli-testdouble',
 
-  included: function included(app) {
-    if (app.app) {
-      app = app.app;
-    }
-    this.app = app;
+  treeForVendor(tree) {
+    var trees = [];
 
-    app.import('bower_components/testdouble/dist/testdouble.js');
-    app.import('vendor/shims/testdouble.js', {
-      type: 'vendor',
-      exports: {
-        'testdouble': ['default']
-      }
+    if (tree) {
+      trees.push(tree)
+    }
+
+    var tdTree = new Funnel(tdDirectory)
+    trees.push(tdTree);
+
+    return mergeTrees(trees);
+  },
+
+  included(app) {
+    var vendor = this.treePaths.vendor;
+
+    app.import(vendor + '/dist/testdouble.js', {
+      type: 'test',
+      using: [
+        { transformation: 'amd', as: 'testdouble' }
+      ]
     });
   }
 };
--- vendor/shims/testdouble.js
+++
@@ -1,9 +0,0 @@
-/* globals td */
-
-define('testdouble', [], function() {
-  "use strict";
-
-  return {
-    'default': td
-  };
-});
